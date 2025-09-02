ea7d7061bfc56cfd422aba37e1db3e0ea1007771
samuel.funck <159692435+samuelFNK@users.noreply.github.com>
2025-09-02 14:42:24 +0200

    Setup basic authentication and a test endpoint that it applies to.

---
+++ src/main/java/com/restassured/restassured/configs/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.restassured.restassured.configs;
+
+import com.restassured.restassured.converters.JwtAuthConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    private final  JwtAuthConverter jwtAuthConverter;
+
+    public SecurityConfig(JwtAuthConverter jwtAuthConverter){
+        this.jwtAuthConverter = jwtAuthConverter;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(csrf -> csrf.disable() )
+                .authorizeHttpRequests(auth ->
+                        auth
+                                .requestMatchers("/api/v2/restassured").hasRole("client1_ADMIN")
+                                .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthConverter))
+                );
+
+        return http.build();
+    }
+
+}
---
+++ src/main/java/com/restassured/restassured/controllers/UserController.java
@@ -0,0 +1,19 @@
+package com.restassured.restassured.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v2")
+public class UserController {
+
+
+    @GetMapping("/posts")
+    public String getAllPosts(){
+        return "Test String For Admins";
+    }
+
+
+}
+
